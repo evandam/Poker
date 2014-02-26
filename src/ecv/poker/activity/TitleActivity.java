5d217dab9f5d2d175dbd7e4e024f4bf8941b2acb
evandam <evandam92@gmail.com>
2014-02-26 01:02:24 -0500

    deleted unnecessary files

--- src/ecv/poker/activity/TitleActivity.java
+++ src/ecv/poker/activity/TitleActivity.java
@@ -16,7 +16,6 @@ public class TitleActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
 		setContentView(titleView);
 	}
 }
